9246cac594c038ec49beeec9f9bb298a2499fbcd
OlavDeng2 <olavkruithof@gmail.com>
2019-09-16 22:41:23 +0200

    created basics for the PlayList Manager

---
+++ Assets/SpotifyClown/Scripts/PlayListManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlayList
+{
+    public string name;
+    public List<AudioClip> songs;
+}
+
+public class PlayListManager : MonoBehaviour
+{
+
+    public List<AudioClip> songs;
+
+    public List<PlayList> listOfPlaylists;
+
+    public void CreatePlaylist(string name)
+    {
+        PlayList playList = new PlayList();
+        playList.name = name;
+        listOfPlaylists.Add(playList);
+    }
+
+
+    public void DeletePlaylist(PlayList playList)
+    {
+        listOfPlaylists.Remove(playList);
+    }
+
+    public void AddToPlayList(PlayList playList, AudioClip song)
+    {
+        playList.songs.Add(song);
+    }
+
+    public void RemoveFromPlaylist(PlayList playList, AudioClip song)
+    {
+        playList.songs.Remove(song);
+    }
+}
